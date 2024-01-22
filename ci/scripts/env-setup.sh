8686fb51e45055b3eaa6cf229aecbea835cde4a4
humeman <camdenjsf@gmail.com>
2024-01-22 13:11:08 -0600

    Maybe CI will work. Wouldn't that be crazy?

--- ci/scripts/env-setup.sh
+++ ci/scripts/env-setup.sh
@@ -2,23 +2,21 @@
 
 set -e
 
-echo "-- Verifying dependencies --"
-apt install -y jq coreutils
-
 echo "-- Expanding host variables --"
 SECRETS="SECRETS_$1"
-JSON=$(echo ${!SECRETS} | base64 --decode)
+JSON=$(echo "${!SECRETS}" | base64 --decode)
 
-export SSH_USER=$(echo $JSON | jq '.user')
-export SSH_HOST=$(echo $JSON | jq '.host')
-echo $JSON | jq '.sudo' > sudo
-echo $JSON | jq '.key' | base64 --decode > id_rsa
+export SSH_USER=$(echo $JSON | jq -r '.user')
+SSH_HOST=$(echo $JSON | jq -r '.host')
+echo $JSON | jq -r '.sudo' > sudo
+echo $JSON | jq -r '.key' | base64 --decode > id_rsa
+echo "" >> id_rsa
 chmod 0600 id_rsa
 
-BUCKET=$(echo $JSON | jq '.bucket')
-echo $JSON | jq '.sa' | base64 --decode > service.json
+BUCKET=$(echo $JSON | jq -r '.bucket')
+echo $JSON | jq -r '.sa' | base64 --decode > service.json
 
-echo $JSON | jq '.vault_k' > vault_k
+echo $JSON | jq -r '.vault_k' > vault_k
 
 unset ${SECRETS}
 unset JSON

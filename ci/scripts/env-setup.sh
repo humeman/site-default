a6ca4cf3d50fc3d8d89d7ab48a006344e3470587
humeman <camdenjsf@gmail.com>
2024-01-22 12:20:25 -0600

    Reworked secrets

--- ci/scripts/env-setup.sh
+++ ci/scripts/env-setup.sh
@@ -2,41 +2,43 @@
 
 set -e
 
-echo "-- Expanding host variables --"
-export SSH_USER="SSH_USER_$1"
-export SSH_KEY="SSH_KEY_$1"
-export SSH_HOST="SSH_HOST_$1"
-export SSH_SUDO="SSH_SUDO_$1"
-export ANSIBLE_VAULT_K="ANSIBLE_VAULT_K_$1"
-export SA="SA_$1"
-export BUCKET="BUCKET_$1"
+echo "-- Verifying dependencies --"
+apt install -y jq coreutils
 
-echo "-- Writing keys --"
-echo "${!SSH_KEY}" > id_rsa
+echo "-- Expanding host variables --"
+SECRETS="SECRETS_$1"
+JSON=$(echo ${!SECRETS} | base64 --decode)
+
+export SSH_USER=$(echo $JSON | jq '.user')
+export SSH_HOST=$(echo $JSON | jq '.host')
+echo $JSON | jq '.sudo' > sudo
+echo $JSON | jq '.key' | base64 --decode > id_rsa
 chmod 0600 id_rsa
-echo "${!SSH_SUDO}" > sudo
-echo "${!ANSIBLE_VAULT_K}" > vault_k
+
+BUCKET=$(echo $JSON | jq '.bucket')
+echo $JSON | jq '.sa' | base64 --decode > service.json
+
+echo $JSON | jq '.vault_k' > vault_k
+
+unset ${SECRETS}
+unset JSON
 
 echo "-- Creating inventory --"
 cat << EOF > inventory
 [servers]
-${!SSH_HOST} ansible_user=${!SSH_USER} ansible_ssh_common_args='-o StrictHostKeyChecking=no'
+${SSH_HOST} ansible_user=${SSH_USER} ansible_ssh_common_args='-o StrictHostKeyChecking=no'
 EOF
 
 echo "-- Authenticating SA --"
-echo "${!SA}" > service.json
 gcloud auth activate-service-account --key-file=service.json
 
 echo "-- Downloading vault --"
-gsutil cp gs://${!BUCKET}/vault .
+gsutil cp gs://${BUCKET}/vault .
 
 echo "-- Cleaning up --"
 rm service.json
 gcloud auth revoke --all
-unset ${SA}
-unset ${BUCKET}
-unset ${ANSIBLE_VAULT_K}
-unset ${SSH_KEY}
-unset ${SSH_SUDO}
+unset BUCKET
+unset SSH_HOST
 
 echo "-- Ready to go! --"

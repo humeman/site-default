f0df51974e0229be129e438e784d656394a7e564
humeman <camdenjsf@gmail.com>
2024-01-22 19:57:25 -0600

    Fix deploy

--- playbooks/files/deploy.sh
+++ playbooks/files/deploy.sh
@@ -10,26 +10,26 @@ readonly TIME=$(date +%s)
 readonly GIT_USER={{ git_user }}
 readonly GIT_PAT={{ git_pat }}
 
-mkdir -p /var/www/$NAME/repo
-chown -R www:www /var/www/$NAME/repo
-git clone https://{{ git_user }}:{{ git_pat }}@{{ git_repo }} "/var/www/$NAME/repo/$TIME"
-chown -R www:www "/var/www/$NAME/repo/$TIME"
-chmod -R 400 /var/www/$NAME/repo/$TIME
+mkdir -p "$DOCROOT/repo"
+chown -R www:www "$DOCROOT/repo"
+git clone https://{{ git_user }}:{{ git_pat }}@{{ git_repo }} "$DOCROOT/repo/$TIME"
+chown -R www:www "$DOCROOT/repo/$TIME"
+chmod -R 400 $DOCROOT/repo/$TIME
 
 # Switch to current version
 old_ver=-1
-if [ -f /var/www/$NAME/repo/version ]; then
-    old_ver=$(cat "/var/www/$NAME/repo/version")
+if [ -f $DOCROOT/repo/version ]; then
+    old_ver=$(cat "$DOCROOT/repo/version")
 fi
 
-echo $TIME > "/var/www/$NAME/repo/version"
-if [ -f "/var/www/$NAME/active" ]; then
-    rm "/var/www/$NAME/active"
+echo $TIME > "$DOCROOT/repo/version"
+if [ -L "$DOCROOT/active" ]; then
+    rm "$DOCROOT/active"
 fi
-ln -s "/var/www/$NAME/repo/$TIME/src" "/var/www/$NAME/active"
-chown -h www:www "/var/www/$NAME/active"
-chmod 500 /var/www/$NAME/repo/$TIME
+ln -s "$DOCROOT/repo/$TIME/src" "$DOCROOT/active"
+chown -h www:www "$DOCROOT/active"
+chmod 500 $DOCROOT/repo/$TIME
 
 if [ $old_ver -ne -1 ]; then
-    rm -rf "/var/www/$NAME/repo/$old_ver"
+    rm -rf "$DOCROOT/repo/$old_ver"
 fi
